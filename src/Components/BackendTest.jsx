a2ac0148b1f17bb9fca1be22929ef20dcb349123
varsha-precisiongrow <varsha@precisiongrow.co.in>
2026-05-22 14:43:29 +0530

    fix render backend1

--- src/Components/BackendTest.jsx
+++ src/Components/BackendTest.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import axios from "axios";
-
-// const BackendTest= () => {
-//   const testApi = async () => {
-//     try {
-//       const res = await axios.post(
-//         "https://coffeeinstitute.co.in/api/create-order"
-//       );
-
-//       console.log(res.data);
-
-//       alert("Backend Connected ✅");
-//     } catch (err) {
-//       console.log(err);
-
-//       alert("Connection Failed ❌");
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={testApi}>
-//         Test Backend
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default BackendTest;
-
 import React from "react";
 import axios from "axios";
 
@@ -36,19 +5,15 @@ const BackendTest = () => {
 
   const testApi = async () => {
     try {
-
       const res = await axios.get(
-        "https://coffeeinstitute.co.in/api/test"
+        "https://coffee-website-zoyk.onrender.com/api/test"
       );
 
       console.log(res.data);
-
       alert("Backend Connected ✅");
 
     } catch (err) {
-
       console.log(err);
-
       alert("Connection Failed ❌");
     }
   };
